52e6f6c8276da88bf44925076b226bfe381730a2
J Skoba <parkinglotlust@gmail.com>
2024-03-30 10:27:25 -0500

    feat: joinWith to reduce trips to database. check processed boolean.

--- source/public_html/index.php
+++ source/public_html/index.php
@@ -1,7 +1,9 @@
 <?php declare(strict_types=1);
 
 /**
- * Display all songs in a table
+ * Display all songs in a table.
+ * 
+ * @author Jordan Skoblenick <parkinglotlust@gmail.com> 2020-03-19
  */
 
 use Propel\Runtime\ActiveQuery\Criteria; 
@@ -9,11 +11,15 @@ use Propel\Runtime\ActiveQuery\Criteria;
 require_once(__DIR__.'/ui.php');
 
 $songs = SongQuery::create()
+	->joinWithEpisode()
 	->useEpisodeQuery()
-		->orderByUploadDate(Criteria::DESC)
+		->filterByProcessed(true)
+		->orderByUploadedDatetime(Criteria::DESC)
 	->endUse()
 	->orderByTrackNumber(Criteria::ASC)
 	->find();
 
-$template->assign('songs', $songs);
+$template->assign([
+    'songs' => $songs
+]);
 $template->display('index.tpl');
