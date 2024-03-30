d6e3357e01dfb65785ccf6e940f198cff76b902b
J Skoba <parkinglotlust@gmail.com>
2024-03-30 10:22:30 -0500

    feat: change to GA4.

--- source/public_html/ui.php
+++ source/public_html/ui.php
@@ -5,11 +5,16 @@ require_once(__DIR__.'/../inc.php');
 header('Content-Type: text/html; charset=utf-8');
 
 // tell template to never cache templates
-// probably want this to = false in production
+// probably want this to = false in production. @todo
 $disableTemplateCaching = true;
 
 // base template object
-$template = new Template(__DIR__.'/../templates/', __DIR__.'/../../cache/templates_compiled/', __DIR__.'/../../cache/templates_cache/', $disableTemplateCaching);
+$template = new Template(
+	__DIR__.'/../templates/', 
+	__DIR__.'/../../cache/templates_compiled/', 
+	__DIR__.'/../../cache/templates_cache/', 
+	$disableTemplateCaching
+);
 
 // auto escape all stuff (dont need {{}} any more!)
 $template->escape_html = true;
@@ -20,5 +25,6 @@ $template->wrapper = 'wrapper.tpl';
 $template->css('index.css');
 $template->css('sortable-theme-light.css');
 
+$template->js('https://www.googletagmanager.com/gtag/js?id='.GA_MEASUREMENT_ID);
 $template->js('ga.js');
 $template->js('sortable.min.js');
